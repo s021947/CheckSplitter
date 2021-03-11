c9ece2167519f287521e864df32c80f3352ca130
Krzysztof Jarocha <s021947@students.lmsd.org>
2021-03-11 12:11:39 -0500

    intial commit

---
+++ CheckSplitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckSplitter
+//
+//  Created by Krzysztof Jarocha (student LM) on 3/11/21.
+//  Copyright © 2021 Krzysztof Jarocha (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
