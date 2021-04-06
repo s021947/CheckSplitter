32fc01576adc4c94a1b104ea9a25704e614ce4c8
ryan rossman <ryan.n.rossman@gmail.com>
2021-04-06 11:22:45 -0400

    testing w welcome screen

--- CheckSplitter/ContentView.swift
+++ CheckSplitter/ContentView.swift
@@ -9,10 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Goodbye, World!")
-    }
+    
+        
+        var body: some View {
+            
+            ZStack{
+            
+            //logo in back
+            
+            //button to scan check
+            Rectangle()
+            .foregroundColor(Color(red:0/255, green: 128/255, blue: 255/255))
+            .edgesIgnoringSafeArea(.all)
+                           
+            Rectangle()
+            .foregroundColor(Color(red: 228/255, green: 195/255, blue: 76/255))
+            .rotationEffect(Angle(degrees: 45))
+            .edgesIgnoringSafeArea(.all)
+            .offset(x: 95, y: 70)
+                
+               // Rectangle()
+              //  .foregroundColor(Color(red:0/255, green: 128/255, blue: 255/255))
+                //.rotationEffect(Angle(degrees: 45))
+                //.edgesIgnoringSafeArea(.all)
+                //.offset(x: 100, y: 20)
+            
+            Text("Welcome").bold()
+                
+            
+                
+            }
+           
+               
+                    
 }
+}
+    
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
